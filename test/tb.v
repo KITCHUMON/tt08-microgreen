1b8b40062da0814927b32e47cd87902e390a60f4
ATHUL <129504550+KITCHUMON@users.noreply.github.com>
2025-12-06 18:41:42 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,6 +1,17 @@
+`default_nettype none
 `timescale 1ns / 1ps
 
-module tb;
+/* This testbench just instantiates the module and makes some convenient wires
+   that can be driven / tested by the cocotb test.py.
+*/
+module tb ();
+
+  // Dump the signals to a VCD file. You can view it with gtkwave.
+  initial begin
+    $dumpfile("tb.vcd");
+    $dumpvars(0, tb);
+    #1;
+  end
     reg clk;
     reg rst_n;
     reg [7:0] ui_in;
@@ -11,6 +22,10 @@ module tb;
     
     // Instantiate DUT
     tt_um_microgreen_bnn user_project (
+`ifdef GL_TEST
+      .VPWR(1'b1),
+      .VGND(1'b0),
+`endif
         .clk(clk),
         .rst_n(rst_n),
         .ena(ena),
@@ -20,84 +35,4 @@ module tb;
         .uio_out(uio_out),
         .uio_oe(uio_oe)
     );
-
-    // Generate 25 MHz clock
-    initial clk = 0;
-    always #20 clk = ~clk;
-
-    // Camera and echo simulation
-    reg vsync, href, pclk, echo;
-    reg [7:0] pixel_data;
-
-    assign uio_in = {vsync, href, pclk, 5'b0000} | {7'b0, echo};
-
-    initial begin
-        $dumpfile("tb.vcd");
-        $dumpvars(0, tb);
-
-        // Reset
-        rst_n = 0;
-        ui_in = 0;
-        vsync = 0; href = 0; pclk = 0; echo = 0;
-        #200;
-        rst_n = 1;
-        #200;
-
-        // Simulate a frame
-        simulate_camera_frame();
-
-        // Simulate ultrasonic echo
-        simulate_ultrasonic_echo(16'd600); // approx 15 cm
-
-        // Wait for BNN output
-        #2000;
-        $display("uo_out: %b", uo_out);
-        $finish;
-    end
-
-    task simulate_camera_frame;
-        integer i;
-        begin
-            // VSYNC pulse
-            vsync = 1;
-            #100;
-            vsync = 0;
-            #100;
-
-            // Simulate a few lines with green pixels
-            for (i = 0; i < 10; i = i + 1) begin
-                href = 1;
-                send_pixel(8'h3C);  // greenish
-                send_pixel(8'hA0);
-                href = 0;
-                #100;
-            end
-
-            // End of frame
-            vsync = 1;
-            #100;
-            vsync = 0;
-        end
-    endtask
-
-    task send_pixel;
-        input [7:0] data;
-        begin
-            pclk = 0;
-            ui_in = data;
-            #20;
-            pclk = 1;
-            #20;
-            pclk = 0;
-        end
-    endtask
-
-    task simulate_ultrasonic_echo;
-        input [15:0] pulse_width;
-        begin
-            echo = 1;
-            #(pulse_width);
-            echo = 0;
-        end
-    endtask
 endmodule
