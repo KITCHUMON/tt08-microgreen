9e1e8407eae0c34ca1a5610236a8819c60fb359c
ATHUL <129504550+KITCHUMON@users.noreply.github.com>
2025-12-06 10:21:25 +0530

    Rename module from tt_um_mitspro to tt_um_microgreen_classifier

--- src/project.v
+++ src/project.v
@@ -6,7 +6,7 @@
 
 `default_nettype none
 
-module tt_um_mitspro (
+module tt_um_microgreen_classifier (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
